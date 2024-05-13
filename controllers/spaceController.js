501efde1862f64cbe798530a275a9b2c2721bb11
AdrianL824 <adrian.lopez.or2408@gmail.com>
2024-05-13 01:10:31 -0400

    Add: Max, min to spaces

--- controllers/spaceController.js
+++ controllers/spaceController.js
@@ -1,13 +1,22 @@
 import spacesModel from "../models/spaceModel.js";
 import slugify from "slugify";
 export const registerSpaceController = async (req, res) => {
-    try {
-      const { name, capacity, block, webaddress } = req.body;
-      //validations
-      if (!name) {
-        return res.send({ error: "Name is Required" });
-      }
-      {/*
+  try {
+    const { name, capacity, minCapacity, maxCapacity, block, webaddress } =
+      req.body;
+    //validations
+    if (!name) {
+      return res.send({ error: "Name is Required" });
+    }
+    if (
+      capacity < minCapacity ||
+      capacity > maxCapacity ||
+      maxCapacity < minCapacity
+    ) {
+      return res.send({ error: "Capacity have inconsistencies" });
+    }
+    {
+      /*
       if (!capacity) {
         return res.send({ message: "capacity is Required" });
       }
@@ -16,62 +25,67 @@ export const registerSpaceController = async (req, res) => {
       }
       if (!webaddress) {
         return res.send({ message: "WebAddress is Required" });
-      }*/}
-      //save
-      const space = await new spacesModel({
-        name, capacity, block, webaddress, slug: slugify(name),
-      }).save();
-  
-      res.status(200).send({
-        success: true,
-        message: "space Register Successfully",
-        space,
-      });
-    } catch (error) {
-      console.log(error);
-      res.status(500).send({
-        success: false,
-        message: "Error in Registeration",
-        error,
-      });
+      }*/
     }
-  };
-  //get all spaces
-  export const spaceControlller = async (req, res) => {
-    try {
-      const space = await spacesModel.find({});
-      res.status(200).send({
-        success: true,
-        message: "All Space List",
-        space,
-      });
-    } catch (error) {
-      console.log(error);
-      res.status(500).send({
-        success: false,
-        error,
-        message: "Error while getting all spaces",
-      });
-    }
-  };
+    //save
+    const space = await new spacesModel({
+      name,
+      capacity,
+      minCapacity,
+      maxCapacity,
+      block,
+      webaddress,
+      slug: slugify(name),
+    }).save();
 
-  //get single space
-  export const singleSpaceController = async (req, res) => {
-    try {
-      const space = await spacesModel.findOne({ name: req.params.slug });
-      res.status(200).send({
-        success: true,
-        message: "Get SIngle Space SUccessfully",
-        space,
-      });
-    } catch (error) {
-      console.log(error);
-      res.status(500).send({
-        success: false,
-        error,
-        message: "Error While getting Single Space",
-      });
-    }
-  };
+    res.status(200).send({
+      success: true,
+      message: "space Register Successfully",
+      space,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in Registeration",
+      error,
+    });
+  }
+};
+//get all spaces
+export const spaceControlller = async (req, res) => {
+  try {
+    const space = await spacesModel.find({});
+    res.status(200).send({
+      success: true,
+      message: "All Space List",
+      space,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error while getting all spaces",
+    });
+  }
+};
 
-
+//get single space
+export const singleSpaceController = async (req, res) => {
+  try {
+    const space = await spacesModel.findOne({ name: req.params.slug });
+    res.status(200).send({
+      success: true,
+      message: "Get SIngle Space SUccessfully",
+      space,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error While getting Single Space",
+    });
+  }
+};
